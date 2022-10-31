f652a57f1aee58c3902fa7bcd063d7792c98a8b5
SuInWoo <wbo1026@naver.com>
2022-11-01 01:20:56 +0900

    SW EA 1859번: 백만 장자 프로젝트 - 출력값 보고 long형 사용 - sample code 보고 하나씩 손으로 해보기

---
+++ src/SW1859.java
@@ -0,0 +1,32 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SW1859 {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("res/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            long rnt = 0;
+            int max = 0;
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            for (int i = n-1; i >= 0; i--) {
+                if (arr[i] > max)
+                    max = arr[i];
+                rnt += max - arr[i];
+            }
+            System.out.printf("#%d %d\n", test_case, rnt);
+
+        }
+        sc.close();
+    }
+}
