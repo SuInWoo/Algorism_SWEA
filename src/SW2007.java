3e0c6c3108d28daba5a063a38a6440ab75426712
SuInWoo <wbo1026@naver.com>
2022-11-01 23:56:23 +0900

    SW EA 2007: 패턴 마디의 길이 - 쉬운 문제였지만, 문제 제대로 안읽어서 헤맴

---
+++ src/SW2007.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SW2007 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            String str = br.readLine();
+            for (int j = 1; j <= 10; j++) {
+                if (str.substring(0, j).equals(str.substring(j, j+j))) {
+                    System.out.printf("#%d %d\n", i, j);
+                    break;
+                }
+            }
+        }
+    }
+}
